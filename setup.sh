1a9798770e2305b5c91fc26792d39f276ef45c10
Natsu2401 <111423869+Natsu2401@users.noreply.github.com>
2022-08-29 20:23:15 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo pacman -S --needed base-devel git rofi ncmpcpp mpd bspwm sxhkd ranger picom neovim dunst zsh xfce4-terminal ruby nodejs npm python-pip xdotool
+sudo pacman -S --needed base-devel git rofi ncmpcpp mpd bspwm sxhkd ranger picom neovim dunst zsh xfce4-terminal ruby nodejs npm python-pip xdotool polybar
 
 # fonts
 npm i sauce-code-pro
